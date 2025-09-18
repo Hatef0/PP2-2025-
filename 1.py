fca5f0a57f4296c061667fd37ac3fe989e2c8ed9
Hatef Chalak <hatefochalak@gmail.com>
2025-09-18 11:56:22 +0500

    Delete 1.py
    Deleted a.py file

--- 1.py
+++
@@ -1,8 +0,0 @@
-# ==============================================
-# Python Syntax
-# ==============================================
-# Indentation is very important in Python.
-# Code blocks are defined by indentation.
-
-if 5 > 2:
-    print("5 is greater than 2")  # This line is inside the block
